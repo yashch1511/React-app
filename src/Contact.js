b7b403b855324065c8ad08cf06ece1db28ff760f
yashch1511 <162914821+yashch1511@users.noreply.github.com>
2024-08-25 17:38:28 +0530

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact(){
+    return(
+        <div className="Contact">
+            <img src="Group 77.png" className="share" />
+            <h1 className="Subscribe">Subscribe to get information, latest news and other
+            interesting offers about Jadoo</h1>
+            <div className="info">
+                <input type="email" className="col" required placeholder="Your email"></input>
+                <button type="submit" className="info-btn">Subscribe</button>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
